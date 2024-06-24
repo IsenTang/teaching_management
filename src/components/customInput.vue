92004b490123b3893a60982273932e6d1cfce55d
Isen <3104255643@qq.com>
2024-06-24 09:25:00 +0800

    feat: init

---
+++ src/components/customInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-input v-bind="attrs" v-on="$listeners"></el-input>
+</template>
+
+<script>
+export default {
+  computed: {
+    attrs() {
+      return {
+        ...this.$attrs,
+      };
+    },
+  },
+  created() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <customInput @focus="onFocus" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import customInput from '@/components/customInput.vue';
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
-    HelloWorld,
+    customInput,
+  },
+  methods: {
+    onFocus() {
+      console.log('focus');
+    },
   },
 };
 </script>
